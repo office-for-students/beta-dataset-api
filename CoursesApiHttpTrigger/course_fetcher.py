a86ec1ed404e0aba93f9a338c41c731ecc6fc407
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-06-21 11:10:50 +0100

    Add doc cleanup code and comments
    Add code to remove the fields Cosmos DB adds to the course document.
    Improve comments.

--- CoursesApiHttpTrigger/course_fetcher.py
+++ CoursesApiHttpTrigger/course_fetcher.py
@@ -6,6 +6,7 @@ import azure.cosmos.errors as errors
 import sys
 
 class CourseFetcher:
+    """Handles retrieving and formatting the requested course from CosmosDB"""
 
     def __init__(self, client, collection_link):
         self.client = client
@@ -28,10 +29,25 @@ class CourseFetcher:
         # TODO investigate partitions and choosing the partition key
         options = { 'enableCrossPartitionQuery': True }
 
-        results_list = list(self.client.QueryItems(self.collection_link, query, options))
-        if not(len(results_list)):
+        courses_list = list(self.client.QueryItems(self.collection_link, query, options))
+        if not(len(courses_list)):
             return None
 
         # TODO: alert here if more than one result
-        return json.dumps(results_list[0])
+        course = courses_list[0]
 
+        tidied_course = CourseFetcher.tidy_course(course)
+        return json.dumps(tidied_course)
+
+
+    @staticmethod
+    def tidy_course(course):
+        """Removes the extra fields Cosmos DB added to the course"""
+
+        keys_to_delete = ['_rid', '_self', '_etag', '_attachments', '_ts']
+        for key in keys_to_delete:
+            try:
+                del course[key]
+            except KeyError:
+                logging.warning(f"Expected key not found: {key}")
+        return course

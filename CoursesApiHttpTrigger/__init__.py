2c0a526f2047b05159bd4517e162c6e7858df657
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-06-21 12:03:25 +0100

    Improve formatting and comments

--- CoursesApiHttpTrigger/__init__.py
+++ CoursesApiHttpTrigger/__init__.py
@@ -5,38 +5,49 @@ import azure.functions as func
 
 from .course_fetcher import CourseFetcher
 
-from .utils import (
-    get_collection_link,
-    get_cosmos_client)
+from .utils import (get_collection_link, get_cosmos_client, get_not_found_json)
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    """Implements the REST API endpoint for getting course documents. 
 
-    name = req.params.get('id')
-    logging.info(f"url is {req.url}")
-    logging.info(f"params is {req.params}")
-    logging.info(f"route_params is {req.route_params}")
-    logging.info(type(req.route_params))
-    
+    The endpoint implemented is:
+        /institutions/{institution_id}/courses/{course_id}/modes/{mode} 
+
+    The API is fully documented in a swagger document within the same repo
+    as this module - https://github.com/office-for-students/beta-dataset-api
+    """
+
+    logging.info('Process a request for a course.')
+    logging.info(f"url: {req.url}")
+    logging.info(f"params: {req.params}")
+    logging.info(f"route_params: {req.route_params}")
+
+    # Put all the parameters together
     params = dict(req.route_params)
-    params['version'] = '1'
+    version = req.params.get('version', '1')
+    params['version'] = version
+    logging.info(f"Parameters: {params}")
 
-    #return func.HttpResponse("Hello")
-    # For now set version to "1"
+    # TODO if not validate(params):
+    #   return bad request error
 
+    # Intialise a CourseFetcher
     client = get_cosmos_client()
     collection_link = get_collection_link()
-
     course_fetcher = CourseFetcher(client, collection_link)
+
+    # Get the course
     course = course_fetcher.get_course(**params)
+
     logging.info(type(course))
     logging.info(course)
 
     if course:
-        return func.HttpResponse(course, status_code=200)
+        return func.HttpResponse(course,
+                                 headers={"Content-Type": "application/json"},
+                                 status_code=200)
     else:
-        return func.HttpResponse(
-             "No course found",
-             status_code=400
-        )
+        return func.HttpResponse(get_not_found_json(),
+                                 headers={"Content-Type": "application/json"},
+                                 status_code=400)

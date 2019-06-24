a1576ebe092b0c411a62c317aab1b696508d9942
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-06-24 17:19:50 +0100

    Rework for param validation

--- CoursesApiHttpTrigger/__init__.py
+++ CoursesApiHttpTrigger/__init__.py
@@ -6,11 +6,10 @@ import azure.functions as func
 
 from .course_fetcher import CourseFetcher
 
-from .utils import (
-    get_collection_link, 
-    get_cosmos_client,
-    get_http_error_response_json, 
-    mandatory_params_present)
+from .utils import (get_collection_link, get_cosmos_client,
+                    get_http_error_response_json)
+
+from .course_param_validator import valid_course_params
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -36,20 +35,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         logging.info(f"Parameters: {params}")
 
         #
-        # Check the request is valid
+        # The params are used in DB queries, so let's do
+        # some basic sanitisation.
         #
+        if not valid_course_params(params):
+            logging.error(f"valid_course_params returned false for {params}")
+            return func.HttpResponse(
+                get_http_error_response_json('Bad Request', 'Parameter Error',
+                                             'Invalid parameter passed'),
+                headers={"Content-Type": "application/json"},
+                status_code=400)
 
-        # Check all mandatory params provided
-        if not mandatory_params_present(
-            ('institution_id', 'course_id', 'mode'), params):
-            return func.HttpResponse(get_http_error_response_json('Bad Request',
-                'argument error', 'missing mandatory argument'),
-                                    headers={"Content-Type": "application/json"},
-                                    status_code=400)
-
-
-        # TODO Add param validation
-
+        logging.info('Passed param validation')
 
         # Intialise a CourseFetcher
         client = get_cosmos_client()
@@ -60,13 +57,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         course = course_fetcher.get_course(**params)
 
         if course:
-            return func.HttpResponse(course,
-                                    headers={"Content-Type": "application/json"},
-                                    status_code=200)
+            return func.HttpResponse(
+                course,
+                headers={"Content-Type": "application/json"},
+                status_code=200)
         else:
-            return func.HttpResponse(get_http_error_response_json('Not Found', 'course', 'Course was not found.'),
-                                    headers={"Content-Type": "application/json"},
-                                    status_code=404)
+            return func.HttpResponse(
+                get_http_error_response_json('Not Found', 'course',
+                                             'Course was not found.'),
+                headers={"Content-Type": "application/json"},
+                status_code=404)
     except Exception as e:
         logging.error(traceback.format_exc())
+
+        # Currently raise so Azure sends back the HTTP 500
         raise e

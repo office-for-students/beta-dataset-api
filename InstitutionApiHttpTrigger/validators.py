07b120378c83035bd00ae55a2ee98baa2f80c1bb
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-30 13:50:47 +0100

    Add validators for institution

---
+++ InstitutionApiHttpTrigger/validators.py
@@ -0,0 +1,52 @@
+import logging
+
+from SharedCode.string_validator import StringValidator
+
+
+def valid_institution_params(params):
+
+    if not mandatory_params_present(
+        ('institution_id', ), params):
+        logging.error(f"Mandatory parameters missing from: {params}")
+        return False
+
+    if not valid_param("institution_id", params['institution_id'], 8, 8,
+                       r'[\d]+$'):
+        return False
+
+    if not valid_param("version", params['version'], 1, 20, r'[\d]+$'):
+        return False
+
+    return True
+
+
+def mandatory_params_present(mandatory_params, params):
+    print(f'mandatory_params is {mandatory_params}')
+    print(f'params is {params}')
+    print(mandatory_params[0])
+    print(mandatory_params[0] in params)
+    return all(k in params for k in mandatory_params)
+
+
+def valid_param(name, param, min_length, max_length, regex):
+    """Test that the param looks reasonable."""
+
+    if not StringValidator.is_valid_type(param):
+        logging.error(f"{name} is an invalid type - expecting string")
+        return False
+
+    string_validator = StringValidator(param,
+                                       min_length=min_length,
+                                       max_length=max_length,
+                                       regex=regex)
+
+    if not string_validator.is_valid_length():
+        logging.error(f"{name} is invalid length {param}")
+        return False
+
+    if not string_validator.valid_chars_only():
+        logging.error(
+            f"{param} the param for {name} contains invalid characters.")
+        return False
+
+    return True

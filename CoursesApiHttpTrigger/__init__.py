c612826652077fee6b38cc1993adfa477a2c8721
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-31 12:35:30 +0100

    Update so uses SharedCode for utils

--- CoursesApiHttpTrigger/__init__.py
+++ CoursesApiHttpTrigger/__init__.py
@@ -6,8 +6,12 @@ import azure.functions as func
 
 from .course_fetcher import CourseFetcher
 
-from .utils import (get_collection_link, get_cosmos_client,
-                    get_http_error_response_json)
+
+from SharedCode.utils import (
+    get_collection_link,
+    get_cosmos_client,
+    get_http_error_response_json,
+)
 
 from .course_param_validator import valid_course_params
 
@@ -23,15 +27,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     """
 
     try:
-        logging.info('Process a request for a course.')
+        logging.info("Process a request for a course.")
         logging.info(f"url: {req.url}")
         logging.info(f"params: {req.params}")
         logging.info(f"route_params: {req.route_params}")
 
         # Put all the parameters together
         params = dict(req.route_params)
-        version = req.params.get('version', '1')
-        params['version'] = version
+        version = req.params.get("version", "1")
+        params["version"] = version
         logging.info(f"Parameters: {params}")
 
         #
@@ -41,33 +45,38 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         if not valid_course_params(params):
             logging.error(f"valid_course_params returned false for {params}")
             return func.HttpResponse(
-                get_http_error_response_json('Bad Request', 'Parameter Error',
-                                             'Invalid parameter passed'),
+                get_http_error_response_json(
+                    "Bad Request", "Parameter Error", "Invalid parameter passed"
+                ),
                 headers={"Content-Type": "application/json"},
-                status_code=400)
+                status_code=400,
+            )
 
-        logging.info('The parameters look good')
+        logging.info("The parameters look good")
 
         # Intialise a CourseFetcher
         client = get_cosmos_client()
-        collection_link = get_collection_link()
+        collection_link = get_collection_link(
+            "AzureCosmosDbDatabaseId", "AzureCosmosDbCoursesCollectionId"
+        )
         course_fetcher = CourseFetcher(client, collection_link)
 
         # Get the course
         course = course_fetcher.get_course(**params)
 
         if course:
-            logging.info(f'Found a course {course}')
+            logging.info(f"Found a course {course}")
             return func.HttpResponse(
-                course,
-                headers={"Content-Type": "application/json"},
-                status_code=200)
+                course, headers={"Content-Type": "application/json"}, status_code=200
+            )
         else:
             return func.HttpResponse(
-                get_http_error_response_json('Not Found', 'course',
-                                             'Course was not found.'),
+                get_http_error_response_json(
+                    "Not Found", "course", "Course was not found."
+                ),
                 headers={"Content-Type": "application/json"},
-                status_code=404)
+                status_code=404,
+            )
 
     except Exception as e:
         logging.error(traceback.format_exc())

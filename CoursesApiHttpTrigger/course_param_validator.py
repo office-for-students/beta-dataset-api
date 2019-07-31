e7ffba4d1fdac8dd434e5c34485bee72f7f089c8
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-07-31 15:55:54 +0100

    Update to pull string_validator from shared code

--- CoursesApiHttpTrigger/course_param_validator.py
+++ CoursesApiHttpTrigger/course_param_validator.py
@@ -1,27 +1,25 @@
 import logging
 
-from .string_validator import StringValidator
+from SharedCode.string_validator import StringValidator
 
 
 def valid_course_params(params):
 
-    if not mandatory_params_present(
-        ('institution_id', 'course_id', 'mode'), params):
+    if not mandatory_params_present(("institution_id", "course_id", "mode"), params):
         logging.error(f"Mandatory parameters missing from: {params}")
         return False
 
-    if not valid_param("institution_id", params['institution_id'], 8, 8,
-                       r'[\d]+$'):
+    if not valid_param("institution_id", params["institution_id"], 8, 8, r"[\d]+$"):
         return False
 
-    if not valid_param("course_id", params['course_id'], 1, 30, r'[\w\~-]+$'):
+    if not valid_param("course_id", params["course_id"], 1, 30, r"[\w\~-]+$"):
         return False
 
-    if not valid_param("mode", params['mode'], 1, 1, r'[123]$'):
+    if not valid_param("mode", params["mode"], 1, 1, r"[123]$"):
         return False
 
     # No upper limit on version; setting to 100 chars should be more than enough!
-    if not valid_param("version", params['version'], 1, 100, r'[\d]+$'):
+    if not valid_param("version", params["version"], 1, 100, r"[\d]+$"):
         return False
 
     return True
@@ -38,18 +36,16 @@ def valid_param(name, param, min_length, max_length, regex):
         logging.error(f"{name} is an invalid type - expecting string")
         return False
 
-    string_validator = StringValidator(param,
-                                       min_length=min_length,
-                                       max_length=max_length,
-                                       regex=regex)
+    string_validator = StringValidator(
+        param, min_length=min_length, max_length=max_length, regex=regex
+    )
 
     if not string_validator.is_valid_length():
         logging.error(f"{name} is invalid length {param}")
         return False
 
     if not string_validator.valid_chars_only():
-        logging.error(
-            f"{param} the param for {name} contains invalid characters.")
+        logging.error(f"{param} the param for {name} contains invalid characters.")
         return False
 
     return True
